9fc4b4190b2db8917b933f1f40a665aaa76c509c
ANANT PATEL <120110880+ANANTPATEL505@users.noreply.github.com>
2025-09-23 13:36:41 +0530

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -1,1 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+//Contract Address : 0x56224337380C1987CD34002265D04BAd34fff83e
+contract noob {
+    
+    address public manager;
+    address payable[] public parti;
 
+    constructor(){
+        manager=msg.sender;
+    }
+
+    receive() external payable {
+        require(msg.value>=0.0001 ether);
+        parti.push(payable(msg.sender));
+    }
+
+    function get()public view returns(uint){
+        require(msg.sender==manager);
+        return address(this).balance;
+    }
+
+    function random()public view returns(uint){
+        return uint(keccak256(abi.encode(block.prevrandao,block.timestamp,parti.length)));
+    }
+
+    function pick() public {
+        require(msg.sender==manager, "Only manager can pick");
+        require(parti.length >=3, "Need at least 3 players");
+        address payable win;
+        uint index=random()%parti.length;
+        win=parti[index];
+        win.transfer(get());
+        parti=new address payable [](0);
+    }
+
+}
